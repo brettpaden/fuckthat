065e80fb1bb2c3c407996b477f94c60f0e3a839c
Colin Stryker <colin@pkt3.com>
2012-07-03 13:48:36 -0700

    Changed to proper logging path

--- chrome3/log.js
+++ chrome3/log.js
@@ -1,7 +1,7 @@
 function log(data) {
     var params = { data: data };
     $.ajax({
-        url: Bummer_Root_Server + '/api/fucks/fuckthat',
+        url: Bummer_Root_Server + '/api/errors/log',
         type: 'POST',
         data: params
     });
