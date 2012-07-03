2268bb22d101325bb63e84eae1ecfa8009e6df00
Colin Stryker <colin@pkt3.com>
2012-07-03 16:36:33 -0700

    Add file to announce presence of chrome plugin

---
+++ chrome3/announce.js
@@ -0,0 +1,4 @@
+// Announce myself to the home website, so we can detect whether the plugin is installed
+$(function() {
+  $('body').append('<div id="bummer_plugin_installed" style="display:none;"></div>');
+});
